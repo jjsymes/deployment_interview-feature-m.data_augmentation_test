7a3050a887bf7cefa8aa703729c36301bb3a23ec
Josh Symes <josh@example.com>
2021-07-24 20:26:09 +0100

    Add fliptb augmentation

--- app/image_augmentations.py
+++ app/image_augmentations.py
@@ -2,7 +2,7 @@ from PIL import Image
 import random
 
 def random_augmentation(image: Image):
-    augmentations = [rotate, fliplr, do_nothing]
+    augmentations = [rotate, fliplr, fliptb, do_nothing]
     func = random.choice(augmentations)
     return func(image)
 
@@ -12,7 +12,8 @@ def do_nothing(image: Image):
 def fliplr(image: Image):
     return image.transpose(Image.FLIP_LEFT_RIGHT)
 
+def fliptb(image: Image):
+    return image.transpose(Image.FLIP_TOP_BOTTOM)
+
 def rotate(image: Image):
     return image.rotate(random.randint(0,365))
-
-
